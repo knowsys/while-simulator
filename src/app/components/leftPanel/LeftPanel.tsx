31efa0bf660bef848579dadd1f7a69e0d9629207
CI <>
2022-01-10 02:38:35 +0000

    Automatic build

--- src/app/components/leftPanel/LeftPanel.tsx
+++
@@ -1,5 +0,0 @@
-import { DraftJSEditor } from './editor/draftJSEditor/DraftJSEditor';
-
-export function LeftPanel() {
-    return <DraftJSEditor />;
-}
--- src/core/parser/Parser.ts
+++
@@ -1,5 +0,0 @@
-import { Statement } from '../ast/Statement';
-
-export abstract class Parser {
-    public abstract parseProgramText(program: string): Statement;
-}
